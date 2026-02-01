93fbdda42ca4db7fb3b05cd10afd86fd1821e20f
celestianguyen <celestianguyenx@gmail.com>
2026-02-02 06:47:54 +0800

    exercise 2

--- src/exercises/Exercise02.java
+++ src/exercises/Exercise02.java
@@ -11,42 +11,42 @@ Each type of information (for example, full name) is stored in a separate array.
 The information of one employee is identified by the same index position in all arrays.
  */
 public class Exercise02 {
+    static Scanner scanner = new Scanner(System.in);
+    private static String[] fullName;
+    private static int[] age;
+    private static String[] gender;
+    private static double[] basicSalary;
+    private static double[] gpa;
+
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+
         int n = readInt(scanner,"Please enter the number of employees whose info you wanna store: ");
-
-        String[] fullName = new String[n];
-        int[] age = new int[n];
-        String[] gender = new String[n];
-        double[] basicSalary = new double[n];
-        double[] gpa = new double[n];
-
+        fullName = new String[n];
+        age = new int[n];
+        gender = new String[n];
+        basicSalary = new double[n];
+        gpa = new double[n];
         for (int i = 0; i < n; i++){
             System.out.println("\n--Input employee " + (i + 1) + " --");
-            inputEmployeeInfo(scanner, i, fullName, age, gender, basicSalary, gpa);
+            inputEmployeeInfo(i);
         }
 
         System.out.println("\n--- Employee List ---");
         for (int i = 0; i < n; i++) {
             System.out.println("Employee #" + (i + 1));
-            System.out.println(" Full name: " + fullName[i]);
-            System.out.println(" Age: " + age[i]);
-            System.out.println(" Gender: " + gender[i]);
-            System.out.println(" Salary: " + formatMoney(basicSalary[i]));
-            System.out.println(" GPA: " + String.format("%.2f", gpa[i]));
+            printEmployeeInfo(i);
         }
 
     }
-    private static void inputEmployeeInfo(Scanner scanner, int i, String[] fullName, int[] age, String[] gender, double[] basicSalary, double[] gpa){
-        fullName[i] = readNonEmptyLine(scanner, "Enter employee's name. Please enter name(s) again!");
+    private static void inputEmployeeInfo(int i){
+        fullName[i] = readNonEmptyLine("Enter employee's name: ");
         age[i] = readInt(scanner, "Please enter employee " + (i + 1) + " age: ");
-        gender[i] = readNonEmptyLine();
-        basicSalary[i] = readDouble();
-        gpa[i] = readDouble();
+        gender[i] = readGender("Enter employee's gender: ");
+        basicSalary[i] = readDouble("Please enter employee's basic salary: ");
+        gpa[i] = readDouble("Please enter employee's gpa on a 10-point scale: ");
 
     }
-    private static void printEmployeeInfo(int i,String[] fullName, double age, String[] gender, double[] basicSalary, double[] gpa){
-        System.out.println("Employee #" + (i + 1));
+    private static void printEmployeeInfo(int i){
         System.out.println("Full name: " + fullName[i]);
         System.out.println("Age: " + age[i]);
         System.out.println("Gender: " + gender[i]);
@@ -54,10 +54,10 @@ public class Exercise02 {
         System.out.println("GPA: " + String.format("%.2f", gpa[i]));
     }
 
-    private static String readGender(Scanner scanner, String prompt) {
+    private static String readGender(String prompt) {
         while (true) {
             System.out.print(prompt);
-            String s = scanner.nextLine().trim();
+            String s = readNonEmptyLine(prompt);
             if (s.equalsIgnoreCase("M") || s.equalsIgnoreCase("Male")) return "Male";
             if (s.equalsIgnoreCase("F") || s.equalsIgnoreCase("Female")) return "Female";
             if (s.equalsIgnoreCase("Other") || s.equalsIgnoreCase("O")) return "Other";
@@ -68,18 +68,18 @@ public class Exercise02 {
     private static String formatMoney(double v) {
         return String.format("%,.2f", v);
     }
-    private static void readNonEmptyLine(){
+    private static String readNonEmptyLine(String target){
         while (true) {
-            System.out.print(Scanner scanner, String prompt);
+            System.out.print(target);
             String s = scanner.nextLine().trim();
             if (!s.isEmpty()) return s;
             System.out.println("  Value cannot be empty.");
         }
     };
-    private static double readDouble(Scanner scanner, String prompt){
+    private static double readDouble(String prompt){
         while (true){
             System.out.print(prompt);
-            double d = scanner.nextDouble();
+            String d = scanner.nextLine();
             try{
                 return Double.parseDouble(d);
 
